e123bc61cc7479d8ad06b004fc0fbf2748f7bc77
CEDRIC DERUE <cedric.derue@altran.com>
2022-09-27 10:38:59 +0200

    change provider conf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  /*cloud {
+    organization = "philit"
+
+    workspaces {
+      name = "gitops-env-demo"
+    }
+  }*/
+
   required_version = ">=1.0"
 
   required_providers {
@@ -15,4 +23,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
