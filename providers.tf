9ba113af33d41bd15ef95eacbf71868c96dd894b
CEDRIC DERUE <cedric.derue@altran.com>
2022-09-29 08:09:04 +0200

    update provider configuration

--- providers.tf
+++ providers.tf
@@ -1,11 +1,4 @@
 terraform {
-  /*cloud {
-    organization = "philit"
-
-    workspaces {
-      name = "gitops-env-demo"
-    }
-  }*/
 
   required_version = ">=1.0"
 
@@ -18,10 +11,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    /*tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
-    }*/
   }
 }
 
